fdaba2791eb5af1a4b60b472511cd53f298333b9
AnandaGaliwango <anandagaliwango@gmail.com>
2025-10-30 10:13:55 +0300

    Update urls.py

--- Backend/backend/payments/urls.py
+++ Backend/backend/payments/urls.py
@@ -8,10 +8,13 @@ urlpatterns = [
     # Payments
     path('pay/pesapal/', views.pesapal_payment, name='pesapal_payment'),
     path('pay/mtn/', views.mtn_momo_payment, name='mtn_momo_payment'),
-    # path('pay/airtel/', views.airtel_money_payment, name='airtel_money_payment'),
+    path('pay/airtel/', views.airtel_money_payment, name='airtel_money_payment'),  # Uncommented
 
     # Payment callbacks / IPNs
     path('pesapal-callback/', views.pesapal_callback, name='pesapal_callback'),
     path('mtn-momo-callback/', views.mtn_momo_callback, name='mtn_momo_callback'),
-    # path('airtel-money-callback/', views.airtel_money_callback, name='airtel_money_callback'),
+
+    # Health check
+    path('health/', views.api_health, name='api_health'),
+    path('', views.api_health, name='api_root'),
 ]
